315774f0a78833261b3c217b98c93433e69d8e6e
acolter12 <acolter@kent.edu>
2022-06-13 17:52:02 -0400

    Began About model.

---
+++ src/main/java/org/wecancodeit/birdwatcher/About.java
@@ -0,0 +1,50 @@
+package org.wecancodeit.birdwatcher;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class About {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String memberBio;
+    private String memberImageUrl;
+
+    public About(){
+
+    }
+
+    public About(String firstName, String lastName, String memberBio, String memberImageUrl){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.memberBio = memberBio;
+        this.memberImageUrl = memberImageUrl;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getMemberBio(){
+        return memberBio;
+    }
+
+    public String getMemberImageUrl(){
+        return memberImageUrl;
+    }
+    
+}
