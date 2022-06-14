a204b59d97e9ecdca98697140e225cc628ce2a13
zibadcoder <emushmala@yahoo.com>
2022-06-14 17:10:41 -0400

    continents html and css

--- src/main/java/org/wecancodeit/birdwatcher/controller/RegionController.java
+++ src/main/java/org/wecancodeit/birdwatcher/controller/RegionController.java
@@ -2,6 +2,7 @@ package org.wecancodeit.birdwatcher.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.birdwatcher.repo.RegionRepository;
 
 import javax.annotation.Resource;
@@ -11,4 +12,10 @@ public class RegionController {
 
     @Resource
     RegionRepository regionRepo;
+    @RequestMapping("/regions")
+    public String displayRegions(){
+
+
+        return "regions";
+    }
 }
