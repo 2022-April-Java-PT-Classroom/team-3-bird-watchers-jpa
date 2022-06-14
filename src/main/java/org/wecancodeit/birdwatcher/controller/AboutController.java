2a614d857c2fdd3f67879db2c4230e8f61f90ce1
acolter12 <acolter@kent.edu>
2022-06-14 18:55:03 -0400

    Created mapping to about.html.

--- src/main/java/org/wecancodeit/birdwatcher/controller/AboutController.java
+++ src/main/java/org/wecancodeit/birdwatcher/controller/AboutController.java
@@ -1,15 +1,21 @@
 package org.wecancodeit.birdwatcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.birdwatcher.repo.AboutRepository;
 
 import javax.annotation.Resource;
 
 @Controller
-@RequestMapping("/about")
 public class AboutController {
 
     @Resource
     private AboutRepository aboutRepo;
+
+    @RequestMapping("/about")
+    public String displayAbout(Model model){
+        model.addAttribute("about", aboutRepo.findAll());
+        return "about";
+    }
 }
