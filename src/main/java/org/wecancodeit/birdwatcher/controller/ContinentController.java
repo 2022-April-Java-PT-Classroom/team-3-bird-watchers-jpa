80a01f897d5e23f8117012a8e0cf2e068583fecd
cmsrimsson <cmsrimsson@gmail.com>
2022-06-16 15:42:32 -0400

    stylesheet for continent created and it's model,repo,and controller too created, Also .css file for all templates are created too

--- src/main/java/org/wecancodeit/birdwatcher/controller/ContinentController.java
+++ src/main/java/org/wecancodeit/birdwatcher/controller/ContinentController.java
@@ -1,6 +1,7 @@
 package org.wecancodeit.birdwatcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.birdwatcher.repo.ContinentRepository;
 
@@ -14,9 +15,8 @@ public class ContinentController {
 
     @RequestMapping("/continents")
 
-    public String displayContinent(){
-
-
+    public String displayContinent(Model model){
+        model.addAttribute("continents", continentRepo.findAll());
         return "continents";
     }
 }
