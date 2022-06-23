8f80e252dcf76aa4b1dd7ddfc372a9a9535a564e
zibadcoder <emushmala@yahoo.com>
2022-06-23 16:46:47 -0400

    connected each country with its image and description

--- src/main/java/org/wecancodeit/birdwatcher/controller/CountryController.java
+++ src/main/java/org/wecancodeit/birdwatcher/controller/CountryController.java
@@ -1,7 +1,9 @@
 package org.wecancodeit.birdwatcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancodeit.birdwatcher.repo.CountryRepository;
 
 
@@ -9,10 +11,13 @@ import javax.annotation.Resource;
 
 @Controller
 public class CountryController {
+    @Resource
+    CountryRepository countryRepo;
 
-    @RequestMapping("templates/country.html")
-    public String displayContinentPointingCountry(){
+    @RequestMapping("templates/country")
+    public String displayContinentPointingCountry(@RequestParam(value ="id") Long id, Model model){
+        model.addAttribute("country", countryRepo.findById(id).get());
 
-        return "country";
+        return "country.html";
     }
 }
---
+++ src/main/java/org/wecancodeit/birdwatcher/populator.java
@@ -0,0 +1,43 @@
+package org.wecancodeit.birdwatcher;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.wecancodeit.birdwatcher.model.Continent;
+import org.wecancodeit.birdwatcher.model.Country;
+import org.wecancodeit.birdwatcher.repo.AboutRepository;
+import org.wecancodeit.birdwatcher.repo.BirdRepository;
+import org.wecancodeit.birdwatcher.repo.ContinentRepository;
+import org.wecancodeit.birdwatcher.repo.CountryRepository;
+
+import javax.annotation.Resource;
+
+@Component
+public class populator implements CommandLineRunner {
+
+    @Resource
+    private ContinentRepository continentStorage;
+    @Resource
+    private CountryRepository countryStorage;
+    @Resource
+    private AboutRepository aboutStorage;
+    @Resource
+    private BirdRepository birdStorage;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Continent continent = new Continent("Africa", "continent rich in various types of beautiful birds");
+        continentStorage.save(continent);
+        Country Tanzania = new Country("Tanzania", "/images/map-of-tanzania.png",
+                "Tanzania is home to some of Africa's most famous national parks" +
+                        " and natural attractions, including majestic Mount Kilimanjaro.",
+                "Tanzania has a lot of attractive places like Katavi National Park.");
+        countryStorage.save(Tanzania);
+        Country Morocco = new Country("Morocco", "/images/map-of-morocco.png",
+                "Morocco is home to the world's largest desert known as the Sahara desert!",
+                "Morocco is home for various sites like the coastal wetlands of Oualidia and " +
+                        "the Souss-Massa National Park.");
+        countryStorage.save(Morocco);
+
+
+    }
+}
