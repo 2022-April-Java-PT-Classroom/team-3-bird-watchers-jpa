ae98eac4b4b177c3fcccbe60ccba057b444357bc
zibadcoder <emushmala@yahoo.com>
2022-06-23 17:15:02 -0400

    created populator for continent and country.linked continents page to each country dynamically

--- src/main/java/org/wecancodeit/birdwatcher/populator.java
+++ src/main/java/org/wecancodeit/birdwatcher/populator.java
@@ -29,11 +29,14 @@ public class populator implements CommandLineRunner {
         continentStorage.save(continent);
         Country Tanzania = new Country("Tanzania", "/images/map-of-tanzania.png",
                 "Tanzania is home to some of Africa's most famous national parks" +
-                        " and natural attractions, including majestic Mount Kilimanjaro.",
+                        " and natural attractions, including majestic Mount Kilimanjaro. It has a lot of attractive sites " +
+                        "that host many types of beautiful birds",
                 "Tanzania has a lot of attractive places like Katavi National Park.");
         countryStorage.save(Tanzania);
         Country Morocco = new Country("Morocco", "/images/map-of-morocco.png",
-                "Morocco is home to the world's largest desert known as the Sahara desert!",
+                "Morocco is home to the world's largest desert known as the Sahara desert! " +
+                        "The country has many sites that are rich " +
+                        "in many unique types of birds.",
                 "Morocco is home for various sites like the coastal wetlands of Oualidia and " +
                         "the Souss-Massa National Park.");
         countryStorage.save(Morocco);
