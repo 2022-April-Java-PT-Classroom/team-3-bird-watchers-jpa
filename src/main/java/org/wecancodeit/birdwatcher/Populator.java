fb1d02eb183bb377d177b5c32d6ddf2d5f461c14
acolter12 <acolter@kent.edu>
2022-06-22 23:09:14 -0400

    Fixed links on other pages' templates for about page. Started css layout for about page.

--- src/main/java/org/wecancodeit/birdwatcher/Populator.java
+++ src/main/java/org/wecancodeit/birdwatcher/Populator.java
@@ -16,30 +16,26 @@ public class Populator implements CommandLineRunner {
     @Resource
     private AboutRepository aboutRepo;
 
-    Map<Long, About> about = new HashMap<>();
-
     @Override
     public void run(String... args) throws Exception {
-//        About farida = new About("Farida", "NDiaye", "Hello, I'm Farida NDiaye. " +
-//                "I have an Associate degree and 2 years+ of experience in Business Management," +
-//                " Biology, Chemistry, and Geology. I am passionate about traveling and discovering." +
-//                " I love trying/learning new skills and taking on professional challenges." +
-//                " My curiosity to understand how all these machines/applications work led me to take" +
-//                " a closer look at the tech industry. So, I'm currently registered at Columbus" +
-//                " State Community College in the Software Developer program and in a Bootcamp at We Can Code IT" +
-//                " to learn Front-end and Back-end development.", "../images/farida.png");
+        About farida = new About("Farida", "NDiaye", "I have an Associate degree and 2 years+ of experience in Business" +
+                " Management. I am passionate about traveling and discovering. I love learning new skills and taking on professional challenges." +
+                " I'm currently learning web development at We Can Code IT", "../images/farida.png");
 
         About zibad = new About("Zibad", "Maldaye", "My name is Zibad and I am full stack Java developer." +
                 " I am passionate for soccer and fitness.", "../images/zibad.jpg");
 
         About adrian = new About("Adrian", "Colter", "I graduated Kent State University in 2018 with a Bachelor of Arts in" +
-                " Communication Studies with an Applied Communication concentrationCurrent full stack web development" +
+                " Communication Studies with an Applied Communication concentration Current full stack web development" +
                 " student at We Can Code IT.","../images/adrian.JPG");
-        About cm = new About("Chuda", "Rimal", "", "../images/chuda.jpg");
+
+        About cm = new About("Chuda", "Rimal", "Front/Back End Web Developer based in Columbus, Ohio.", "../images/chuda.jpg");
+
         About serena = new About("Serena", "Howard", "bio","../images/serena.jpg");
+
         About jimmy = new About("Jimmy", "Negron", "bio", "../images/jimmy.jpg");
 
-//        aboutRepo.save(farida);
+        aboutRepo.save(farida);
         aboutRepo.save(zibad);
         aboutRepo.save(adrian);
         aboutRepo.save(cm);
